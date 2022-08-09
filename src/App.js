b5138f7074fb9e8b0f4ff7ff39c7c7cfc9c9f530
cphalder <91915610+cphalder@users.noreply.github.com>
2022-08-09 22:59:06 +0100

    Added new header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
@@ -8,6 +9,10 @@ export default function App() {
     <div className="App">
       <h1>React Router</h1>
       <Router>
+        <header>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
